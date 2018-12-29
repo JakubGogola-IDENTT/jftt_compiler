0ab96a9442d9a4d82eea6f2cbab96b4657c745f7
Jakub Gogola <jakubgogola97@gmail.com>
2018-12-29 10:48:45 +0100

    generating constant algorithm finished

--- excercises/const_gen.py
+++ excercises/const_gen.py
@@ -1,5 +1,6 @@
 import sys
 
+
 class ConstantGenerator:
 
     def __init__(self, const):
@@ -10,10 +11,13 @@ class ConstantGenerator:
         self.const = const
         self.instr_cost = {
             'INC': 1,
-            'ADD': 5
+            'ADD': 5,
+            'HALF': 1,
+            'SUB': 5,
+            'COPY': 5,
+            'PUT': 100
         }
         self.output_code = None
-
 
     def gen_code(self):
         # Const is 0 or 1
@@ -31,6 +35,7 @@ class ConstantGenerator:
         code_inc = []
         cost_inc = 0
         code_inc.append('SUB B B')
+        cost_inc += self.instr_cost['SUB']
 
         for _ in range(0, self.const):
             code_inc.append('INC B')
@@ -38,27 +43,77 @@ class ConstantGenerator:
         
         code_inc.append('PUT B')
         code_inc.append('HALT')
+        cost_inc += self.instr_cost['PUT']
 
         # Generate const using ADD and INC
         code_mixed = []
         cost_mixed = 0
+
         code_mixed.append('SUB B B')
+        cost_mixed += self.instr_cost['SUB']
 
         # How many '2' is in const
         # distr = int(self.const / 2)
 
-        # Generate '2'
+        # Generate '2' in register B
         code_mixed.append('INC B')
         code_mixed.append('INC B')
         cost_mixed += 2 * self.instr_cost['INC']
+
+        # Value in register B
         tmp = 2
 
-        while True:
-            tmp *=  2
-            if tmp <= self.const:
-                code_mixed.append('ADD B B')
-                cost_inc += self.instr_cost['ADD']
+        while tmp + tmp <= self.const:
+            tmp += tmp
+            code_mixed.append('ADD B B')
+            cost_mixed += self.instr_cost['ADD']
 
+        # Half of value in register B
+        code_mixed.append('SUB C C')
+        code_mixed.append('COPY C B')
+        code_mixed.append('HALF C')
+        cost_mixed += self.instr_cost['SUB']
+        cost_mixed += self.instr_cost['COPY']
+        cost_mixed += self.instr_cost['HALF']
+
+        half = tmp / 2
+
+        while half != 2:
+            if tmp + half <= self.const:
+                tmp += half
+                code_mixed.append('ADD B C')
+                cost_mixed += self.instr_cost['ADD']
+
+            if tmp == self.const:
+                break
+            
+            code_mixed.append('HALF C')
+            cost_mixed += self.instr_cost['HALF']
+            half /= 2
+
+        while tmp != self.const:
+            code_mixed.append('INC B')
+            cost_mixed += self.instr_cost['INC']
+            tmp += 1
+
+        code_mixed.append('PUT B')
+        code_mixed.append('HALT')
+        cost_mixed += self.instr_cost['PUT']
+
+        # Check which solution is chepaer
+        if cost_inc <= cost_mixed:
+            self.output_code = code_inc
+            print('Incrementing is faster')
+        else:
+            self.output_code = code_mixed
+            print('Adding and incrementing is faster')
+
+        print('******* COSTS *******')
+        print('Incrementing: ', cost_inc)
+        print('Adding and incrementing: ', cost_mixed)
+        print('*********************')
+
+        # END OF FUNCTION
         self.write_to_file()
 
 
---
+++ excercises/run_const.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python3 const_gen.py
+./maszyna-rejestrowa-cln const.mr
